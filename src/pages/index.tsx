655b9cbaf581fe63e9636b464b5be8e4b8e3544b
Andre Souza <souza.andre1993@gmail.com>
2021-06-13 10:43:04 -0300

    complete index load more button functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,17 +36,27 @@ export default function Home(): JSX.Element {
   }, [data]);
 
   // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
 
   // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
       <Header />
-      {/* <pre>{JSON.stringify(formattedData, null, 2)}</pre> */}
+      {/* <pre>{JSON.stringify(hasNextPage, null, 2)}</pre> */}
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
-        <Button onClick={() => fetchNextPage()}>Load more</Button>
+        {hasNextPage && (
+          <Button mt="40px" onClick={() => fetchNextPage()}>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
